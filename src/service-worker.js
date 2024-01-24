2687cd372205a01567f7185299f798502cbf7a50
karimemmonteiro <contatokarimem@gmail.com>
2024-01-24 14:55:14 -0300

    add imagem desconectado ao pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,18 +1,34 @@
-/* eslint-disable no-restricted-globals */
-
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Rota para imagens, utilizando CacheFirst
 registerRoute(
   ({ request }) => request.destination === 'image',
-  new CacheFirst()
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      // Personalize os plugins conforme necessário
+    ],
+  })
 );
 
+// Rota para scripts e estilos, utilizando StaleWhileRevalidate
 registerRoute(
   ({ request }) => request.destination === 'script' ||
                   request.destination === 'style',
   new StaleWhileRevalidate()
+);
+
+// Rota específica para a imagem desconectado.svg
+registerRoute(
+  ({ url }) => url.pathname.includes('desconectado.svg'),
+  new CacheFirst({
+    cacheName: 'desconectado-images',
+    plugins: [
+      // Personalize os plugins conforme necessário
+    ],
+  })
 );
